31dd6ed6da522092c97c68782a4a074cd70f4c0f
IamIsthill <bercasiocharles14@gmail.com>
2025-12-31 09:56:01 +0800

    feat(tabs): added init home screen and bottom tab

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import "@/assets/global.css";
+
+export default function Index() {
+  return (
+    <View
+    // style={{
+    //   flex: 1,
+    //   justifyContent: "center",
+    //   alignItems: "center",
+    // }}
+    // className="flex-1 bg-black"
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
