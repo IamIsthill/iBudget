c5d7d27dfcfd5c278d031e83349ca40f42a446a9
IamIsthill <bercasiocharles14@gmail.com>
2026-01-06 13:50:36 +0800

    feat(migration): implement migration error handling and loading state in layout

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,27 +1,8 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import "@/src/assets/global.css";
 import { AccountWidget } from "@/src/features/account";
-import { useMigration } from "@/src/db";
 
 export default function Index() {
-  const { error, success } = useMigration();
-
-  if (error) {
-    return (
-      <View>
-        <Text>Migration error: {error.message}</Text>
-      </View>
-    );
-  }
-
-  if (!success) {
-    return (
-      <View>
-        <Text>Migration is in progress...</Text>
-      </View>
-    );
-  }
-
   return (
     <View>
       <AccountWidget />
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,25 @@
 import { Stack } from "expo-router";
+import { useMigration } from "@/src/db";
+import { Text, View } from "react-native";
 
 export default function RootLayout() {
+  const { error, success } = useMigration();
+
+  if (error) {
+    return (
+      <View>
+        <Text>Migration error: {error.message}</Text>
+      </View>
+    );
+  }
+
+  if (!success) {
+    return (
+      <View>
+        <Text>Migration is in progress...</Text>
+      </View>
+    );
+  }
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
